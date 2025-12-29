d4fa286787c711d7cc0ba2d04a4bb35d228dd502
Ahmad Hassan <bsai24007@itu.edu.pk>
2025-12-29 10:47:32 +0500

    DataStore

---
+++ backend/core/DataStore.h
@@ -0,0 +1,151 @@
+#pragma once
+#include <string>
+#include "storage/RecordFile.h"
+#include "storage/BPlusTree.h"
+#include "storage/IdGenerator.h"
+#include "core/KeyUtil.h"
+class DataStore{
+public:
+    IdGenerator ids;
+    RecordFile usersR;
+    RecordFile postsR;
+    RecordFile commentsR;
+    RecordFile requestsR;
+    RecordFile threadNodesR;
+    RecordFile threadMetaR;
+    BPlusTree usersById;
+    BPlusTree usersByUsername;
+    BPlusTree postsById;
+    BPlusTree postsByUserTime;
+    BPlusTree followingByFollower;
+    BPlusTree followersByFollowee;
+    BPlusTree followReqByToTime;
+    BPlusTree followReqByFromTo;
+    BPlusTree likesByUserPost;
+    BPlusTree repostsByUserPost;
+    BPlusTree commentsById;
+    BPlusTree commentsByPostTime;
+    BPlusTree postsByHashtagRank;
+    BPlusTree publicPostsByTrend;
+    BPlusTree threadNodeById;
+    BPlusTree threadChildrenByParentTime;
+    BPlusTree threadMetaByRootId;
+    BPlusTree threadsByPopularity;
+    void openAll(const string &Dir){
+
+        usersR.open(Dir + "/users.rec");
+        postsR.open(Dir + "/posts.rec");
+        commentsR.open(Dir + "/comments.rec");
+        requestsR.open(Dir + "/follow_requests.rec");
+        threadNodesR.open(Dir + "/thread_nodes.rec");
+        threadMetaR.open(Dir + "/thread_meta.rec");
+        usersById.open(Dir + "/idx_users_by_id.bpt");
+        usersByUsername.open(Dir + "/idx_users_by_username.bpt");
+        postsById.open(Dir + "/idx_posts_by_id.bpt");
+        postsByUserTime.open(Dir + "/idx_posts_by_user_time.bpt");
+        followingByFollower.open(Dir + "/idx_following_by_follower.bpt");
+        followersByFollowee.open(Dir + "/idx_followers_by_followee.bpt");
+        followReqByToTime.open(Dir + "/idx_req_by_to_time.bpt");
+        followReqByFromTo.open(Dir + "/idx_req_by_from_to.bpt");
+        likesByUserPost.open(Dir + "/idx_likes_by_user_post.bpt");
+        repostsByUserPost.open(Dir + "/idx_reposts_by_user_post.bpt");
+        commentsById.open(Dir + "/idx_comments_by_id.bpt");
+        commentsByPostTime.open(Dir + "/idx_comments_by_post_time.bpt");
+        postsByHashtagRank.open(Dir + "/idx_posts_by_hashtag_rank.bpt");
+        publicPostsByTrend.open(Dir + "/idx_public_posts_by_trend.bpt");
+        threadNodeById.open(Dir + "/idx_thread_node_by_id.bpt");
+        threadChildrenByParentTime.open(Dir + "/idx_thread_children_by_parent_time.bpt");
+        threadMetaByRootId.open(Dir + "/idx_thread_meta_by_root.bpt");
+        threadsByPopularity.open(Dir + "/idx_threads_by_popularity.bpt");
+        ids.open(Dir + "/ids.bin");
+
+
+    }
+    string kUserId(int64_t userId) {
+        return "U:" + FixedInt(userId, 20);
+
+    }
+    string kUsername(const string &username) {
+        return "UN:" + username;
+
+    }
+
+    string kPostId(int64_t postId) {
+        return "P:" + FixedInt(postId, 20);
+
+    }
+
+    string DataStore::kPostUserTime(int64_t authorId, int64_t timestamp, int64_t postId) {
+        return "PUT:" + FixedInt(authorId, 20) + ":" + invTimeKey(timestamp) + ":" + FixedInt(postId, 20);
+
+    }
+
+    string DataStore::kFollowFF(int64_t followerId, int64_t followeeId) {
+        return "FF:" + FixedInt(followerId, 20) + ":" + FixedInt(followeeId, 20);
+
+    }
+
+    
+    string DataStore::kFollowFE(int64_t followeeId, int64_t followerId) {
+        return "FE:" + FixedInt(followeeId, 20) + ":" + FixedInt(followerId, 20);
+
+    }
+
+    string DataStore::kReqToTime(int64_t toUserId, int64_t timestamp, int64_t reqId) {
+        return "FRTO:" + FixedInt(toUserId, 20) + ":" + FixedInt(timestamp, 20) + ":" + FixedInt(reqId, 20);
+
+    }
+
+    string DataStore::kReqFromTo(int64_t fromUserId, int64_t toUserId) {
+        return "FRFT:" + FixedInt(fromUserId, 20) + ":" + FixedInt(toUserId, 20);
+
+    }
+
+    string DataStore::kLike(int64_t userId, int64_t postId) {
+        return "L:" + FixedInt(userId, 20) + ":" + FixedInt(postId, 20);
+
+    }
+    string DataStore::kRepost(int64_t userId, int64_t postId) {
+        return "R:" + FixedInt(userId, 20) + ":" + FixedInt(postId, 20);
+
+    }
+    string DataStore::kCommentId(int64_t commentId) {
+        return "C:" + FixedInt(commentId, 20);
+
+    }
+
+    string DataStore::kCommentPostTime(int64_t postId, int64_t timestamp, int64_t commentId) {
+        return "CPT:" + FixedInt(postId, 20) + ":" + FixedInt(timestamp, 20) + ":" + FixedInt(commentId, 20);
+
+    }
+
+
+    string DataStore::kHashtagRank(const string &tag, int score, int64_t timestamp, int64_t postId) {
+        return "HT:" + tag + ":" + invScoreKey(score) + ":" + invTimeKey(timestamp) + ":" + FixedInt(postId, 20);
+
+    }
+
+    string DataStore::kPublicTrend(int trendScore, int64_t timestamp, int64_t postId) {
+        return "TR:" + invScoreKey(trendScore) + ":" + invTimeKey(timestamp) + ":" + FixedInt(postId, 20);
+
+    }
+    string DataStore::kThreadNodeId(int64_t nodeId) {
+        return "TN:" + FixedInt(nodeId, 20);
+
+    }
+
+    string DataStore::kThreadChild(int64_t parentNodeId, int64_t timestamp, int64_t childNodeId) {
+        return "TC:" + FixedInt(parentNodeId, 20) + ":" + FixedInt(timestamp, 20) + ":" + FixedInt(childNodeId, 20);
+
+    }
+
+    string DataStore::kThreadMeta(int64_t rootId) {
+        return "TM:" + FixedInt(rootId, 20);
+
+    }
+
+    string DataStore::kThreadPopularity(int threadScore, int64_t lastUpdated, int64_t rootId) {
+        return "TP:" + invScoreKey(threadScore) + ":" + invTimeKey(lastUpdated) + ":" + FixedInt(rootId, 20);
+
+    }
+};
--- backend/core/KeyUtil.h
+++ backend/core/KeyUtil.h
@@ -1,7 +1,40 @@
 #pragma once
 #include<cstdint>
+#include<chrono>
+#include<string>
 using namespace std;
 
 int64_t nowUnixSeconds(){
-    return 
+    return chrono::duration_cast<chrono::seconds>(chrono::system_clock::now().time_since_epoch()).count();
+
 }
+
+string FixedInt(int64_t num,int width){
+    string s= to_string(num);
+    while(s.size()<width){
+        s="0"+s;
+    }
+    return s;
+
+}
+string invTimeKey(int64_t t) {
+    const int64_t Largest = 9999999999;
+    int64_t inver = Largest - t;
+    if (inver < 0) {
+        inver = 0;
+
+    }
+    return FixedInt(inver, 10);
+}
+
+
+string invScoreKey(int t) {
+    const int Largest = 999999999;
+    int inver = Largest - t;
+    if (inver < 0) {
+        inver = 0;
+
+    }
+    return FixedInt(inver, 10);
+}
+
